e3a13f621bb06fb2f011e0abcf126a28f2c35be9
liyang <1922802352@qq.com>
2019-11-06 11:51:03 +0800

    资源页面JS

---
+++ src/main/resources/static/js/resource.js
@@ -0,0 +1,43 @@
+$(function(){
+    $.get("/wt/getResource.json",function(result){
+        var vedio="";
+        var music="";
+        var epub="";
+        //循环影视
+        $.each(result.vedio,function(index,value){
+
+            vedio+="  <li class=layui-timeline-item\" onclick=\"hrefGo('"+value.linkAddres+"')\">\n" +
+                "  <i class=\"layui-icon layui-timeline-axis\"></i>\n" +
+                "   <div class=\"layui-timeline-content layui-text\"><h3 class=\"layui-timeline-title\"  style=\"color: #0C0C0C;\" >"+value.linkByname+"</h3>" +
+                "<p  style=\"font-size: 2px;\">"+value.linkDescribe+"</p></div></li><hr style=\"size: 50px;;align-content: center; background-color: #1c7430;\">";
+
+        });
+        //循环music
+        $.each(result.music,function(index,value){
+
+            music+="  <li class=layui-timeline-item\" onclick=\"hrefGo('"+value.linkAddres+"')\">\n" +
+                "  <i class=\"layui-icon layui-timeline-axis\"></i>\n" +
+                "   <div class=\"layui-timeline-content layui-text\"><h3 class=\"layui-timeline-title\"  style=\"color: #0C0C0C;\" >"+value.linkByname+"</h3>" +
+                "<p  style=\"font-size: 2px;\">"+value.linkDescribe+"</p></div></li><hr style=\"size: 50px;;align-content: center; background-color: #1c7430;\">";
+
+        });
+        //循环epub
+        $.each(result.epub,function(index,value){
+
+            epub+="  <li class=layui-timeline-item\" onclick=\"hrefGo('"+value.linkAddres+"')\">\n" +
+                "  <i class=\"layui-icon layui-timeline-axis\"></i>\n" +
+                "   <div class=\"layui-timeline-content layui-text\"><h3 class=\"layui-timeline-title\"  style=\"color: #0C0C0C;\" >"+value.linkByname+"</h3>" +
+                "<p  style=\"font-size: 2px;\">"+value.linkDescribe+"</p></div></li><hr   style=\"size: 50px;;align-content: center; background-color: #1c7430;\">";
+
+        });
+        $("#vedio").html(vedio);
+        $("#music").html(music);
+        $("#epub").html(epub);
+
+
+
+    });
+    hrefGo=function(s){
+        window.location.href=s;
+    }
+});
