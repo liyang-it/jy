5f318d5bbfb61a294f56d3ed9778236faa338fe3
liyang <1922802352@qq.com>
2019-11-06 11:47:01 +0800

    新增 获取资源方法

--- src/main/java/com/jy/controller/wordTextController.java
+++ src/main/java/com/jy/controller/wordTextController.java
@@ -1,8 +1,10 @@
 package com.jy.controller;
 
+import com.jy.entity.link;
 import com.jy.entity.wordText;
 import com.jy.result.Result;
 import com.jy.result.bindResultError;
+import com.jy.service.dictCache;
 import com.jy.service.wordTextService;
 import com.jy.util.getValue;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,9 @@ import com.jy.config.Validated.*;
 
 import javax.validation.Valid;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 留言控制器
@@ -56,11 +60,34 @@ public class wordTextController extends baseController<wordText>{
         if(br.isBind()){
             return errorResult(br.getMessage());
         }
-        text.setCreatedtime(new Date());
-        text.setOrnotimg(1);
-        text.setUserid(null);
-        text.setWordcontent(getValue.getlineContent(text.getWordcontent()));
-        textService.insertWordText(text);
+        try {
+            text.setCreatedtime(new Date());
+            text.setOrnotimg(1);
+            text.setUserid(null);
+            text.setWordcontent(getValue.getlineContent(text.getWordcontent()));
+            textService.insertWordText(text);
+        }catch (Exception e) {
+            return errorResult(e.getMessage());
+
+        }
+
         return successResult("打卡成功");
+    }
+    @RequestMapping("/dateTest.json")
+    @ResponseBody
+    public List<link> dateTest(){
+        return dictCache.CacheVedio;
+    }
+
+    //获取资源
+    @RequestMapping("/getResource.json")
+    @ResponseBody
+    public Map<String,List<link>> getResource(){
+        Map<String,List<link>> map=new HashMap<String,List<link>>();
+
+        map.put("vedio", dictCache.CacheVedio);
+        map.put("music", dictCache.CacheMusic);
+        map.put("epub", dictCache.CacheEpub);
+        return map;
     }
 }
