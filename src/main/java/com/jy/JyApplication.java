b2602041965f8be8886423c3231ca0328c4723ca
liyang <1922802352@qq.com>
2020-08-25 10:42:05 +0800

    使用war形式

--- src/main/java/com/jy/JyApplication.java
+++ src/main/java/com/jy/JyApplication.java
@@ -4,6 +4,8 @@ import com.jy.util.reloadSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -21,13 +23,18 @@ import java.io.Serializable;
 @Configuration
 @MapperScan("com.jy.mapper")
 @EnableTransactionManagement
-public class JyApplication {
+public class JyApplication extends SpringBootServletInitializer {
 
   @Bean
   public reloadSource reloadSource() {
     return new reloadSource();
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    // 注意这里要指向原先用main方法执行的Application启动类
+    return builder.sources(JyApplication.class);
+  }
 
   public static void main(String[] args) {
 
